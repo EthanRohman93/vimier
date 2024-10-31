f6ec07280ee5d2d85dfb48d240e4896153c265b8
EthanRohman93 <ethanmrohman@gmail.com>
2024-10-31 11:22:30 -0500

    added scripts for dev env

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# update packages
+sudo apt update
+sudo apt install -y curl
+# install node/nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+wait
+nvm install node
+# install pyenv
+curl https://pyenv.run | bash
+wait
+pyenv update
+pyenv install 3.12.5
+# create pyenv venv
+wait
+pyenv virtualenv 3.12.5 openai_env
+pyenv activate openai_env
+# install requirements
+pip install -r requirements.txt
+# deactivate venv
+pyenv deactivate
 # Run vim plugin install
 ./plugins.sh
 # Run fzf install
